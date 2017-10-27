fb0fd6e1ed2eee2f223d4041775e7bcfd17d5407
raziel <Raziel@codefresh.net>
2017-10-27 13:33:23 -0700

    fixing CF-4508

--- unit_test.sh
+++ unit_test.sh
@@ -3,7 +3,7 @@ npm test
 export UNIT_COV=80%
 export UNIT=true
 echo UNIT_COV=$UNIT_COV
-echo UNIT=UNIT
+echo UNIT=$UNIT
 echo UNIT_COV=$UNIT_COV >> $CF_VOLUME_PATH/env_vars_to_export
 echo UNIT=$UNIT >> $CF_VOLUME_PATH/env_vars_to_export
 
