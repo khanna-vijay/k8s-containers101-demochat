d7041159e11b4fd31b66b9afb98723986de2e8f0
Dan <tdangarfield@gmail.com>
2018-04-03 12:13:26 -0700

    add 404 error catch

--- app.js
+++ app.js
@@ -285,3 +285,10 @@ mongoose.connect(settings.database.uri, function(err) {
         startApp();
     });
 });
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
