67a05bde1758c1a9a69712874da12b5177ea0de4
Dan <tdangarfield@gmail.com>
2018-04-03 12:36:03 -0700

    Add 404 path debug info to logs

--- app.js
+++ app.js
@@ -290,7 +290,8 @@ mongoose.connect(settings.database.uri, function(err) {
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
-  console.log("404 Error - Starting Trace");
+  console.log("404 '" + req.path + "' Not found - Starting Trace");
+  console.log(req);
   console.log(err);
   next(err);
 });
